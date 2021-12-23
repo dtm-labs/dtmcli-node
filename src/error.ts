4bd0e3b8480277ac4a00631d1c3a56073a08e4a0
zcong1993 <zhangcong1992@gmail.com>
2021-12-23 16:24:28 +0800

    refactor some code and add tccFromQuery helper function

---
+++ src/error.ts
@@ -0,0 +1,1 @@
+export class DTMError extends Error {}
--- src/tcc.ts
+++ src/tcc.ts
@@ -1,18 +1,26 @@
+import type { ParsedUrlQuery } from 'querystring';
+import axios, { AxiosResponse, AxiosInstance } from 'axios'
 import { IdGenerator, genGid, checkStatus } from './id'
-import axios, { AxiosResponse } from 'axios'
+import { DTMError } from './error'
 
 export class Tcc {
   IdGen: IdGenerator
   dtm: string
   gid: string
+  dtmClient: AxiosInstance
+
   constructor(dtmUrl: string, gid: string) {
     this.dtm = dtmUrl
     this.gid = gid
     this.IdGen = new IdGenerator()
+    this.dtmClient = axios.create({
+      baseURL: dtmUrl
+    })
   }
-  async callBranch(body: any, tryUrl: string, confirmUrl: string, cancelUrl: string): Promise<AxiosResponse> {
+
+  async callBranch<T = any>(body: any, tryUrl: string, confirmUrl: string, cancelUrl: string): Promise<AxiosResponse<T>> {
     let branchId = this.IdGen.newBranchId()
-    let { status } = await axios.post(this.dtm + "/registerTccBranch", {
+    let { status } = await this.dtmClient.post("/registerTccBranch", {
       "gid": this.gid,
       "branch_id": branchId,
       "trans_type": "tcc",
@@ -43,23 +51,33 @@ export async function tccGlobalTransaction(dtmUrl: string, cb: TccCallback): Pro
     trans_type: "tcc",
   }
   try {
-    let { status } = await axios.post(tcc.dtm + "/prepare", tbody)
+    let { status } = await tcc.dtmClient.post("/prepare", tbody)
     checkStatus(status)
     await cb(tcc)
-    await axios.post(tcc.dtm + "/submit", tbody)
+    await tcc.dtmClient.post("/submit", tbody)
   } catch (e) {
     console.error(e)
-    await axios.post(tcc.dtm + "/abort", tbody)
+    await tcc.dtmClient.post("/abort", tbody)
     return ""
   }
   return tcc.gid
 }
 
-export async function tccFromReq(dtmUrl: string, gid: string, branchId: string): Promise<Tcc> {
+export function tccFromReq(dtmUrl: string, gid: string, branchId: string) {
   if (!dtmUrl || !gid || !branchId) {
-    throw `bad req info for tcc dtm: ${dtmUrl} gid: ${gid} branchId: ${branchId}`
+    throw new DTMError(`bad req info for tcc dtm: ${dtmUrl} gid: ${gid} branchId: ${branchId}`)
   }
   let tcc = new Tcc(dtmUrl, gid)
   tcc.IdGen = new IdGenerator(branchId)
+  return tcc
+}
+
+export function tccFromQuery(dtmUrl: string, query: ParsedUrlQuery) {
+  const { gid, branch_id: branchId } = query
+  if (!dtmUrl || !gid || !branchId) {
+    throw new DTMError(`bad req info for tcc dtm: ${dtmUrl} gid: ${gid} branchId: ${branchId}`)
+  }
+  const tcc = new Tcc(dtmUrl, gid as string)
+  tcc.IdGen = new IdGenerator(branchId as string)
   return tcc
 }
